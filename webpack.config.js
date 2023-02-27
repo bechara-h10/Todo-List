e50c25fc86d78d66b12cb4997f2767c99ab5f3ff
bechara-h10 <bechara.hosri1@gmail.com>
2023-02-27 16:56:01 +0200

    Final Version

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry:{
-    main: path.resolve(__dirname,'src/index.js'),
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
   },
-  output:{
-    path: path.resolve(__dirname, 'dist'),
+  output: {
+    path: path.resolve(__dirname, 'dist/index.html'),
     filename: '[name].[contenthash].js',
     clean: true,
-    assetModuleFilename:'[name][ext]',
+    assetModuleFilename: '[name][ext]',
   },
-  devtool:'inline-source-map',
-  devServer:{
-    static: path.resolve(__dirname,'dist'),
-    port:5001,
-    open:true,
-    hot:true
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 5001,
+    open: true,
+    hot: true,
   },
-  module:{
-    rules:[
-      {test:/\.css$/,use:['style-loader','css-loader']},
-      {test:/\.js$/, exclude:/node_modules/,use:{
-        loader:'babel-loader',
-        options:{
-          presets:['@babel/preset-env']
-        }
-      }}
-    ]
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
-      title:'Todo List',
-      filename:'index.html',
-      template:path.resolve(__dirname,'src/temp.html')
-    })
-  ]
+      title: 'Todo List',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/temp.html'),
+    }),
+  ],
 }
