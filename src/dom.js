8458fdb59495331ccbc2aff5bffa87aa7d3494e3
bechara-h10 <bechara.hosri1@gmail.com>
2023-02-27 17:58:28 +0200

    Final Version with mobile view

--- src/dom.js
+++ src/dom.js
@@ -1,53 +1,56 @@
-import TodoList from "./todolist"
-import Project from "./project"
-import Todo from "./todo"
-import Storage from "./storage"
-import { format } from "date-fns"
+import TodoList from './todolist'
+import Project from './project'
+import Todo from './todo'
+import Storage from './storage'
+import { format } from 'date-fns'
 let switcher = 0
-let currentProject;
+let currentProject
 
-class Dom{
-  static showProjects(todoList){
+class Dom {
+  static showProjects(todoList) {
     const projectsDiv = document.querySelector('.projects-container')
-    const defaultProjectDiv = document.querySelector('.default-project-container')
+    const defaultProjectDiv = document.querySelector(
+      '.default-project-container'
+    )
     const inboxProjectDiv = defaultProjectDiv.querySelector('.inbox-container')
     const todayProjectDiv = defaultProjectDiv.querySelector('.today-container')
-    const thisWeekProjectDiv = defaultProjectDiv.querySelector('.this-week-container')
+    const thisWeekProjectDiv = defaultProjectDiv.querySelector(
+      '.this-week-container'
+    )
     projectsDiv.innerText = ''
     inboxProjectDiv.innerText = ''
     todayProjectDiv.innerText = ''
     thisWeekProjectDiv.innerText = ''
-    todoList.projects.forEach(project => {
+    todoList.projects.forEach((project) => {
       const projectTitle = document.createElement('h3')
       const projectLogo = document.createElement('div')
       projectLogo.classList.add('project-logo')
       const xLogo = document.createElement('p')
       xLogo.innerHTML = ' <i class="fa-solid fa-xmark"></i>'
       xLogo.classList.add('logo-x')
-      if(project.title == 'Inbox'){
+      if (project.title == 'Inbox') {
         projectLogo.innerHTML = `<i class="fa-solid fa-inbox"></i>`
         projectTitle.innerText = project.title
         inboxProjectDiv.appendChild(projectLogo)
         inboxProjectDiv.appendChild(projectTitle)
         inboxProjectDiv.classList.add('project-container')
-      } else if(project.title == 'Today'){
+      } else if (project.title == 'Today') {
         projectLogo.innerHTML = `<i class="fa-solid fa-calendar-day"></i> `
         projectTitle.innerText = project.title
         todayProjectDiv.appendChild(projectLogo)
         todayProjectDiv.appendChild(projectTitle)
         todayProjectDiv.classList.add('project-container')
-      } else if(project.title == 'This Week'){
+      } else if (project.title == 'This Week') {
         projectLogo.innerHTML = `<i class="fa-solid fa-calendar-week"></i>`
         projectTitle.innerText = project.title
         thisWeekProjectDiv.appendChild(projectLogo)
         thisWeekProjectDiv.appendChild(projectTitle)
         thisWeekProjectDiv.classList.add('project-container')
-      }
-      else {
+      } else {
         projectLogo.innerHTML = `<i class="fa-solid fa-list-check"></i>`
         const projectDiv = document.createElement('div')
         projectDiv.classList.add('project-container')
-        projectTitle.innerText = project.title 
+        projectTitle.innerText = project.title
         projectDiv.appendChild(projectLogo)
         projectDiv.appendChild(projectTitle)
         projectDiv.appendChild(xLogo)
@@ -59,22 +62,25 @@ class Dom{
       }
     })
     const projects = document.querySelectorAll('.project-container')
-    projects.forEach(project => project.onclick = (e) => {
-      currentProject = project.innerText
-      Dom.showProjectContent(todoList,project.innerText)
-      console.log(currentProject)
-    })
+    projects.forEach(
+      (project) =>
+        (project.onclick = (e) => {
+          currentProject = project.innerText
+          Dom.showProjectContent(todoList, project.innerText)
+          console.log(currentProject)
+        })
+    )
     this.addProjectOption(todoList)
     Storage.saveTodoList(todoList)
   }
 
-  static showProjectContent(todoList,projectTitle){
+  static showProjectContent(todoList, projectTitle) {
     const project = todoList.getProject(projectTitle)
     const projectTitleDiv = document.querySelector('.project-title')
     const todosDiv = document.querySelector('.todos-container')
     projectTitleDiv.innerText = project.title
     todosDiv.innerText = ''
-    project.todos.forEach(todo => {
+    project.todos.forEach((todo) => {
       const todoButton = document.createElement('button')
       const leftPart = document.createElement('div')
       const rightPart = document.createElement('div')
@@ -84,10 +90,12 @@ class Dom{
       xLogo.innerHTML = '<i class="fa-solid fa-xmark"></i>'
       xLogo.classList.add('logo-x')
       todoTitle.innerText = todo.title
-      todo.dueDate == '' ? todoDueDate.innerText = 'No Date' : todoDueDate.innerText = todo.dueDate
+      todo.dueDate == ''
+        ? (todoDueDate.innerText = 'No Date')
+        : (todoDueDate.innerText = todo.dueDate)
       xLogo.onclick = () => {
-        todoList.deleteTodo(currentProject,todo.title)
-        this.showProjectContent(todoList,projectTitle)
+        todoList.deleteTodo(currentProject, todo.title)
+        this.showProjectContent(todoList, projectTitle)
       }
       todoButton.classList.add('todo-button')
       leftPart.classList.add('left-part-todo')
@@ -99,19 +107,19 @@ class Dom{
       todoButton.appendChild(rightPart)
       todosDiv.appendChild(todoButton)
       todoTitle.onclick = () => {
-          this.displayTitleChangeInput(todoList,leftPart)
-          todoTitle.onclick = () => null
-        }
+        this.displayTitleChangeInput(todoList, leftPart)
+        todoTitle.onclick = () => null
+      }
       todoDueDate.onclick = () => {
-        this.displayDateChangeInput(todoList,todoDueDate,todo.title)
+        this.displayDateChangeInput(todoList, todoDueDate, todo.title)
         todoDueDate.onclick = () => null
       }
-      })
+    })
     this.addTodoOption(todoList)
     Storage.saveTodoList(todoList)
   }
 
-  static addProjectOption(todoList){
+  static addProjectOption(todoList) {
     const nav = document.querySelector('nav')
     const addProjectDiv = document.querySelector('.add-project-container')
     addProjectDiv.innerText = ''
@@ -120,13 +128,13 @@ class Dom{
     addProjectButton.innerHTML = `<i class="fa-solid fa-plus"></i> Add Project`
     addProjectDiv.appendChild(addProjectButton)
     nav.appendChild(addProjectDiv)
-    addProjectButton.onclick = ()=> {
+    addProjectButton.onclick = () => {
       switcher = 1
-      Dom.displayInputOption(todoList,addProjectDiv)
+      Dom.displayInputOption(todoList, addProjectDiv)
     }
   }
 
-  static addTodoOption(todoList){
+  static addTodoOption(todoList) {
     const content = document.getElementById('content')
     const addTodoDiv = document.querySelector('.add-todo-container')
     addTodoDiv.innerText = ''
@@ -134,14 +142,14 @@ class Dom{
     addTodoButton.classList.add('add-todo-button')
     addTodoButton.innerHTML = `<i class="fa-solid fa-plus"></i> Add Todo`
     addTodoDiv.appendChild(addTodoButton)
-    addTodoButton.onclick = ()=> {
+    addTodoButton.onclick = () => {
       switcher = 0
-      Dom.displayInputOption(todoList,addTodoDiv)
+      Dom.displayInputOption(todoList, addTodoDiv)
     }
     Storage.saveTodoList(todoList)
   }
 
-  static displayInputOption(todoList,element){
+  static displayInputOption(todoList, element) {
     const input = document.createElement('input')
     input.type = 'text'
     input.id = 'add'
@@ -156,19 +164,19 @@ class Dom{
     buttonsContainer.appendChild(addButton)
     buttonsContainer.appendChild(cancelButton)
     cancelButton.onclick = () => {
-      if(switcher == 1){
+      if (switcher == 1) {
         Dom.addProjectOption(todoList)
       } else {
         Dom.addTodoOption(todoList)
       }
     }
     addButton.onclick = () => {
-      if(switcher == 1){
-      todoList.addProject(input.value)
+      if (switcher == 1) {
+        todoList.addProject(input.value)
         Dom.showProjects(todoList)
       } else {
-        todoList.addTodo(currentProject,input.value)
-        Dom.showProjectContent(todoList,currentProject)
+        todoList.addTodo(currentProject, input.value)
+        Dom.showProjectContent(todoList, currentProject)
       }
     }
     element.innerText = ''
@@ -176,7 +184,7 @@ class Dom{
     element.appendChild(buttonsContainer)
     Storage.saveTodoList(todoList)
   }
-  static displayTitleChangeInput(todoList,element){
+  static displayTitleChangeInput(todoList, element) {
     const title = element.querySelector('h3')
     const titleValue = title.innerText
     const project = todoList.getProject(currentProject)
@@ -188,17 +196,17 @@ class Dom{
     title.innerText = ''
     title.appendChild(input)
     document.onkeydown = (e) => {
-      if(e.key == 'Enter'){
+      if (e.key == 'Enter') {
         title.innerText = input.value
         todo.title = title.innerText
         input.remove()
-        this.showProjectContent(todoList,currentProject)
+        this.showProjectContent(todoList, currentProject)
       }
     }
     Storage.saveTodoList(todoList)
   }
 
-  static displayDateChangeInput(todoList,element,todoTitle){
+  static displayDateChangeInput(todoList, element, todoTitle) {
     const project = todoList.getProject(currentProject)
     const todo = project.getTodo(todoTitle)
     const input = document.createElement('input')
@@ -207,37 +215,49 @@ class Dom{
     element.innerText = ''
     element.appendChild(input)
     document.onkeydown = (e) => {
-      if(e.key == 'Enter'){
+      if (e.key == 'Enter') {
         todo.dueDate = input.value
         todoList.addToToday(todo)
         todoList.addToThisWeek(todo)
-        this.showProjectContent(todoList,currentProject)
-        
+        this.showProjectContent(todoList, currentProject)
       }
+    }
+    input.onblur = () => {
+      todo.dueDate = input.value
+      todoList.addToToday(todo)
+      todoList.addToThisWeek(todo)
+      this.showProjectContent(todoList, currentProject)
     }
     Storage.saveTodoList(todoList)
   }
 
-  static isToday(date){
-    return date == format(new Date(),'yyyy-MM-dd')
+  static isToday(date) {
+    return date == format(new Date(), 'yyyy-MM-dd')
   }
 
   static isDateInThisWeek(date) {
-    const todayObj = new Date();
-    const todayDate = todayObj.getDate();
-    const todayDay = todayObj.getDay();
-  
+    const todayObj = new Date()
+    const todayDate = todayObj.getDate()
+    const todayDay = todayObj.getDay()
+
     // get first date of week
-    const firstDayOfWeek = new Date(todayObj.setDate(todayDate - todayDay));
-  
+    const firstDayOfWeek = new Date(todayObj.setDate(todayDate - todayDay))
+
     // get last date of week
-    const lastDayOfWeek = new Date(firstDayOfWeek);
-    lastDayOfWeek.setDate(lastDayOfWeek.getDate() + 6);
-  
+    const lastDayOfWeek = new Date(firstDayOfWeek)
+    lastDayOfWeek.setDate(lastDayOfWeek.getDate() + 6)
+
     // if date is equal or within the first and last dates of the week
-    return date >= format(firstDayOfWeek,'yyyy-MM-dd') && date <= format(lastDayOfWeek,'yyyy-MM-dd');
+    return (
+      date >= format(firstDayOfWeek, 'yyyy-MM-dd') &&
+      date <= format(lastDayOfWeek, 'yyyy-MM-dd')
+    )
   }
-  
+
+  static showNavBar() {
+    const nav = document.querySelector('nav')
+    nav.classList.toggle('show')
+  }
 }
 
 export default Dom
--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import Project from './project'
 import Dom from './dom'
 import Storage from './storage'
 
+const navBarShow = document.querySelector('.nav-bar-show')
+navBarShow.addEventListener('click', Dom.showNavBar)
 const todoList = Storage.getTodoList()
 todoList.checkToday()
 todoList.checkThisWeek()
 Dom.showProjects(todoList)
-
-
-
-
